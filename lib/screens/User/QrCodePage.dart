3223fff5ec6f49c95cda8d1abd1156670b864b3f
marcelluneza <mgluneza@up.edu.ph>
2023-06-07 20:12:56 +0800

    feat: save qr w/ user, status and date to gallery

--- lib/screens/User/QrCodePage.dart
+++ lib/screens/User/QrCodePage.dart
@@ -1,9 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:provider/provider.dart';
-import '../../providers/AuthProvider.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import '../../providers/UserDetailListProvider.dart';
 import '../../models/UserDetail.dart';
+
+// import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 class QrCodePage extends StatefulWidget {
   const QrCodePage({Key? key}) : super(key: key);
@@ -13,6 +18,14 @@ class QrCodePage extends StatefulWidget {
 }
 
 class _QrCodePageState extends State<QrCodePage> {
+  ScreenshotController _screenshotController = ScreenshotController();
+
+  @override
+  void initState() {
+    // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView()
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -41,42 +54,60 @@ class _QrCodePageState extends State<QrCodePage> {
         backgroundColor: Colors.teal[200],
       ),
       backgroundColor: Colors.teal[100],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _screenshotController
+              .capture(delay: Duration(milliseconds: 10))
+              .then((capturedImage) async {
+            final result = await ImageGallerySaver.saveImage(capturedImage!);
+            print(result);
+          }).catchError((onError) {
+            print(onError);
+          });
+        },
+        child: Icon(Icons.file_download_outlined),
+      ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(height: screenHeight * .1),
-
-            Center(
-              child: QrImage(
-                data: "$name\n$studentNo\n$status",
-                backgroundColor: Color.fromRGBO(128, 203, 196, 1),
-                // ignore: deprecated_member_use
-                foregroundColor: Color.fromRGBO(0, 77, 64, 1),
-                size: screenWidth * .5,
+            Screenshot(
+              controller: _screenshotController,
+              child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      child: QrImageView(
+                        data: "$name\n$studentNo\n$status",
+                        backgroundColor: Color.fromRGBO(128, 203, 196, 1),
+                        // ignore: deprecated_member_use
+                        foregroundColor: Color.fromRGBO(0, 77, 64, 1),
+                        size: screenWidth * .5,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.all(5.0),
+                    child: Center(
+                        child:
+                            Text("Name: ${user?.firstName} ${user?.lastName}")),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Center(child: Text("Status: ${user?.status}")),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5.0),
+                    child:
+                        Center(child: Text("Entry Date: ${user?.latestEntry}")),
+                  ),
+                ],
               ),
             ),
-
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              padding: const EdgeInsets.all(5.0),
-              child: Center(
-                  child: Text("Name: ${user?.firstName} ${user?.lastName}")),
-            ),
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              child: Center(child: Text("Status: ${user?.status}")),
-            ),
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              child: Center(child: Text("Entry Date: ${user?.latestEntry}")),
-            ),
             SizedBox(height: 20),
-            // Padding(
-            //   padding: const EdgeInsets.all(20.0),
-            //   child: Center(child: Text("INSERT MORE EME")),
-            // ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -87,4 +118,18 @@ class _QrCodePageState extends State<QrCodePage> {
       ),
     );
   }
+
+  // Future<dynamic> ShowCapturedWidget(
+  //     BuildContext context, Uint8List capturedImage) {
+  //   return showDialog(
+  //     useSafeArea: false,
+  //     context: context,
+  //     builder: (context) => Scaffold(
+  //       appBar: AppBar(
+  //         title: Text("Captured widget screenshot"),
+  //       ),
+  //       body: Center(child: Image.memory(capturedImage)),
+  //     ),
+  //   );
+  // }
 }
